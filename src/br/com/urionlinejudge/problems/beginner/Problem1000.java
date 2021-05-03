858fb0b17410c2fb1da4862b0adbe6fa5912e272
rsvieira <ramonsv89@gmail.com>
2021-05-02 21:42:26 -0300

    Problem #1000 solved.

---
+++ src/br/com/urionlinejudge/problems/beginner/Problem1000.java
@@ -0,0 +1,12 @@
+package br.com.urionlinejudge.problems.beginner;
+
+/**
+ * @author rsvieira
+ * email - devrsvieira@gmail.com
+ * @date 02/05/2021 21:18:00
+ */
+public class Problem1000 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
