d39fcab5571aca616919d687ceaa7d7618742c3d
rsvieira <ramonsv89@gmail.com>
2021-05-03 11:41:36 -0300

    Problem #1003 solved.

--- src/br/com/urionlinejudge/problems/beginner/Problem1002.java
+++ src/br/com/urionlinejudge/problems/beginner/Problem1002.java
@@ -1,14 +1,11 @@
 package br.com.urionlinejudge.problems.beginner;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
 import java.util.Scanner;
 
 /**
  * @author rsvieira
  * email - devrsvieira@gmail.com
- * @date 03/05/2021 11:06:38
+ * @date 03/05/2021 11:38:25
  */
 
 public class Problem1002 {
@@ -16,17 +13,13 @@ public class Problem1002 {
 	public static void main(String[] args) {
 		
 		try (Scanner scanner = new Scanner(System.in)) {
-			Double n = 3.14159;
-			Double raio;
-			Double A;
+			int A; 
+			int B;
 			
-			raio = scanner.nextDouble();
-			A = n * (raio * raio);
+			A = scanner.nextInt();
+			B = scanner.nextInt();
 
-			DecimalFormat dec = new DecimalFormat("#0.0000");
-			dec.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
-
-			System.out.println("A=" + dec.format(A));
+			System.out.println("SOMA = " + (A + B));
 		}
 	}
 }
