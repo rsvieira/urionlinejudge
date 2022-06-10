f6c7a55d90c560092d45a4e4e676241341f445d5
Ramon <devrsvieira@gmail.com>
2022-06-10 20:19:37 -0300

    Problem1016 resolved.

---
+++ src/br/com/urionlinejudge/problems/beginner/Problem1016.java
@@ -0,0 +1,16 @@
+package br.com.urionlinejudge.problems.beginner;
+
+import java.util.Scanner;
+
+/**
+ * @author devrsvieira
+ * @email devrsvieira@gmail.com
+ * @date 10/06/22
+ */
+public class Problem1016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt()*2 + " minutos");
+    }
+
+}
