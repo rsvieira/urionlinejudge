ad43b4ba0b6c30f6a5e2ad41a81ed7708b1e3cc5
Ramon <devrsvieira@gmail.com>
2022-06-10 21:12:31 -0300

    Problem1017 solved.

---
+++ src/br/com/urionlinejudge/problems/beginner/Problem1017.java
@@ -0,0 +1,19 @@
+package br.com.urionlinejudge.problems.beginner;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * @author devrsvieira
+ * @email devrsvieira@gmail.com
+ * @date 10/06/22
+ */
+public class Problem1017 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double horas = (double) scanner.nextInt();
+        Double distancia = (double) scanner.nextInt();
+        System.out.println(new DecimalFormat("#,###0.000").format(Double.valueOf((horas * distancia) / 12)));
+    }
+}
