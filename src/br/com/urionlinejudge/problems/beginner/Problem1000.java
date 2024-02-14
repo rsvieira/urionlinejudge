01309c657c5e48957d22ad416186aef497c03a99
Ramon Vieira <devrsvieira@gmail.com>
2024-02-14 13:39:22 -0300

    teste

--- src/br/com/urionlinejudge/problems/beginner/Problem1000.java
+++ src/br/com/urionlinejudge/problems/beginner/Problem1000.java
@@ -5,6 +5,7 @@ package br.com.urionlinejudge.problems.beginner;
  * email - devrsvieira@gmail.com
  * @date 02/05/2021 21:18:00
  */
+
 public class Problem1000 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
